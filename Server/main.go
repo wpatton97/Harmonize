1f22c886c85b91120316c054421d0de41a43a7e7
Brandon Burke <bcburke93@gmail.com>
2019-10-26 14:36:02 -0500

    merge

--- Server/main.go
+++ Server/main.go
@@ -4,24 +4,14 @@ import (
 	"fmt"
 	"hackathon/routes"
 	"log"
-<<<<<<< HEAD
-
-	. "github.com/Ragex04/Harmonize/Server/api/indexroute.go"
-	. "github.com/Ragex04/Harmonize/Server/api/routes.go"
-=======
 	"net/http"
 
 	"github.com/gorilla/mux"
->>>>>>> master
 )
 
 const port = 8080
 
 func main() {
-<<<<<<< HEAD
-	log.Printf("YEET\n")
-	log.Printf(routes.b())
-=======
 	router := mux.NewRouter()
 	routes := routes.Routes()
 	for path, handler := range routes {
@@ -31,5 +21,4 @@ func main() {
 	log.Printf("Listening on port: %d", 8080)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 
->>>>>>> master
 }
