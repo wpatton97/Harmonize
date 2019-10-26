0e7f5289a8bfd4c66a0ea99b31ef70009b990cd4
Brandon Burke <bcburke93@gmail.com>
2019-10-26 15:24:36 -0500

    added api go files

---
+++ Server/api/chat/chat.go
@@ -0,0 +1,15 @@
+package chat
+
+import (
+	"net/http"
+)
+
+const staticPath string = "./static/"
+
+func Get(w http.ResponseWriter, r *http.Request) {
+
+	js := "{\"test\":\"Werks\"}"
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
+}
--- Server/routes/routes.go
+++ Server/routes/routes.go
@@ -1,7 +1,13 @@
 package routes
 
 import (
+	"hackathon/api/available"
+	"hackathon/api/channels"
+	"hackathon/api/chat"
 	"hackathon/api/index"
+	"hackathon/api/nowplaying"
+	"hackathon/api/users"
+	"hackathon/api/vote"
 	"net/http"
 )
 
@@ -9,6 +15,11 @@ func Routes() map[string]func(http.ResponseWriter, *http.Request) {
 	allRoutes := make(map[string]func(http.ResponseWriter, *http.Request))
 
 	allRoutes["/"] = index.Index
-
+	allRoutes["/api/available"] = available.Get
+	allRoutes["/api/channels"] = channels.Get
+	allRoutes["/api/nowplaying"] = nowplaying.Get
+	allRoutes["/api/users"] = users.Get
+	allRoutes["/api/vote"] = vote.Get
+	allRoutes["/api/chat"] = chat.Get
 	return allRoutes
 }
